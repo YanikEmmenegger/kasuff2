7b42951d92b75443fdc3ce2908f82033a6ba7614
Yanik <emmenegger@yanik.pro>
2024-11-02 15:34:51 +0100

    BETA VERSION 0.2.2 (Support me)

---
+++ restart.sh
@@ -0,0 +1,15 @@
+# script to restart the server -> docker compsoe down, git pull, docker-compose up --build -d
+
+# stop the server
+docker-compose down
+
+# pull the latest changes
+git pull
+#enter password
+1
+
+# start the server
+docker-compose up --build -d
+
+#check the status
+docker-compose ps
